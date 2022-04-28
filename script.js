ed9ef54501c6417e40a3d36c28f3eff887a86270
Diego <dpardoblacutt@gmail.com>
2022-04-28 18:20:35 -0400

    Add gameRound() and roundWinner() to call to a game round and determine who won

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // a function that creates a computer play -> he sends to you a rock, paper or scissors
 // a function that creates your play -> a popup window if the user doesn't input a valid move
 // a function that is a round game between computer and you
-// a function that decides who won the game !
+// a function that decides who won the round!
 // a function that games 5 rounds and decide winner
+// a function that decides who won the game!
 
-
+function gameRound(){ //function where the computer and the human battles to death at rps :D
+    let computerSelection = getCPUPlay();
+    console.log(computerSelection);
+    let humanSelection = getHumanPlay();
+    roundWinner(computerSelection, humanSelection);
+}
 
 function getCPUPlay(){
     let computerMove = Math.floor(Math.random()*(3)+1);
@@ -19,10 +25,12 @@ function getCPUPlay(){
 
 function getHumanPlay(){
     let goodMove = false;
+    let humanMove = "";
     while(goodMove==false){
-        let humanMove = prompt("Please enter your move:");
+        humanMove = prompt("Please enter your move:");
         goodMove = validateMove(humanMove.toLowerCase());
     }
+    return humanMove;
 }
 
 function validateMove(move){
@@ -39,13 +47,31 @@ function validateMove(move){
 }
 
 function goodMove(){
-    alert("Good move!");
+    alert("You selected your move. Let's see what evils CPU does");
     return true;
 }
 
 function badMove(){
     alert("Bad move! Please try again:");
     return false;
+}
+
+function roundWinner(cpuPlay, humanPlay){
+    if (cpuPlay === humanPlay){
+        console.log("Tie game");
+    }else if (cpuPlay === "rock" && humanPlay === "scissors"){
+        console.log("Rock beats scissors. Computer won this round!");
+    }else if (cpuPlay === "rock" && humanPlay === "paper"){
+        console.log("Paper beats rock. You won this round!");
+    }else if (cpuPlay === "paper" && humanPlay === "rock"){
+        console.log("Paper beats rock. Computer won this round!");
+    }else if (cpuPlay === "paper" && humanPlay === "scissors"){
+        console.log("Scissors beats paper. You won this round!");
+    }else if (cpuPlay === "scissors" && humanPlay === "rock"){
+        console.log("Rock beats scissors. You won this round!");
+    }else if (cpuPlay === "scissors" && humanPlay === "paper"){
+        console.log("Scissors beats paper. Computer won this round!");
+    }
 }
 
 // make game case-insensitive
