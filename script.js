18e45c665d66e6f7968963e914c4ced926f00bc9
Diego <dpardoblacutt@gmail.com>
2022-04-28 11:53:22 -0400

    Comment pseudocode plan to develop game

---
+++ script.js
@@ -0,0 +1,10 @@
+// a function that creates a computer play -> he sends to you a rock, paper or scissors
+// a function that creates your play
+// a function that takes computers game and your game
+// a function that decides who won the game !
+// a function that envolves a single game
+
+// make game case-insensitive
+// make a return of the game
+
+// happy codding :leocheers:
