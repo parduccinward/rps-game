0c43b230bad4e3e8039f791d91338d78df228dd9
Diego <dpardoblacutt@gmail.com>
2022-04-28 16:25:09 -0400

    Add computerPlay() function for computer rock, paper or scissors move

--- script.js
+++ script.js
@@ -4,6 +4,21 @@
 // a function that decides who won the game !
 // a function that envolves a single game
 
+function computerPlay(){
+    let randomValue = Math.floor(Math.random()*(3)+1);
+    if(randomValue===1){
+        return "rock";
+    }else if (randomValue===2){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+
+
+
+
+
 // make game case-insensitive
 // make a return of the game
 
