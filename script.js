27a7fb446e526bc39777ca3c99eed11458aaeecc
Diego <dpardoblacutt@gmail.com>
2022-04-28 16:54:16 -0400

    Add getHumanPlay() function with a while that works until a goodMove boolean

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 // a function that creates a computer play -> he sends to you a rock, paper or scissors
-// a function that creates your play
+// a function that creates your play -> a popup window if the user doesn't input a valid move
 // a function that takes computers game and your game
 // a function that decides who won the game !
 // a function that envolves a single game
 
-function computerPlay(){
-    let randomValue = Math.floor(Math.random()*(3)+1);
-    if(randomValue===1){
+function getCPUPlay(){
+    let computerMove = Math.floor(Math.random()*(3)+1);
+    if(computerMove===1){
         return "rock";
-    }else if (randomValue===2){
+    }else if (computerMove===2){
         return "paper";
     }else{
         return "scissors";
     }
 }
 
-
-
+function getHumanPlay(){
+    while(goodMove==false){
+        let humanMove = prompt("Please enter your move:");
+        goodMove = validateMove(humanMove.toLowerCase());
+    }
+}
 
 
 // make game case-insensitive
